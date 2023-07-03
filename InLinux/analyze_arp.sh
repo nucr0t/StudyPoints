e20c56202cf1926b5201a77862e942db2bbd488e
rusal <rusalekperof@outlook.com>
2023-07-03 23:01:35 +0300

    Add info

--- InLinux/analyze_arp.sh
+++ InLinux/analyze_arp.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+#The script to search for an abnormal number of ARP-queries.
+#Commands builtin in shell(bash) - non user commands
+#read - read one line [-a] - to array,  [-r] - escape backslash
+
 LOG=$1
-COUNT=11  #TOP ip adresses with arp requests
+TOP=11  #TOP ip adresses with arp requests
 IP_REGX='([[:digit:]]{1,3}\.+){3}([[:digit:]]){1,3}'
 
 
+#Counting the number of events per minute. 
+#Minute - containing the second with the most requests. Printing out the results.
 events_counter () {
-	head -$1 $attacker | tail -n1 | read -r -a numb
+	sed -n $1\p $attacker | read -r -a numb
 	if [ "${numb[0]}" -gt 2 ]; then
 		ALL_MIN=
 		minute=$(echo "${numb[1]}" | grep --only-matching --extended-regexp '[0-9]{2}T[0-9]{2}:[0-9]{2}')
@@ -17,8 +23,8 @@ events_counter () {
 		echo -e "\tHOST $(basename $attacker) made $ALL_MIN arp requests at $minute"
 		#sed -n -i "s/${minute}/" "/g" $attacker #Delete used minutes
 	fi
-
 }
+
 
 mkdir ./find
 #Get source ip and timestamp
@@ -29,7 +35,7 @@ cut --fields=2,3 --delimiter=, $LOG > ./temp_log.tmp
 grep --only-matching --extended-regexp $IP_REGX ./temp_log.tmp | sort | uniq --count --repeated | sort --key=1nr > ./result_ip.tmp
 
 #Counter sorting requests in one second
-for (( i=1; i<COUNT; i=i+1 )); do
+for (( i=1; i<TOP; i=i+1 )); do
 	ip=$(sed --silent "${i}p" ./result_ip.tmp | grep --only-matching --extended-regexp $IP_REGX)
 	grep --extended-regexp "${ip}\"" ./temp_log.tmp | cut --fields=1 --delimiter=, > ./time_$ip.tmp
 	grep --only-matching --extended-regexp '[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}' ./time_$ip.tmp | sort \
@@ -38,7 +44,8 @@ done
 
 set +m  #Enable monitor mode for lastpipe
 shopt -s lastpipe  #Disable subshell for save var
-#Events is one minute
+
+#Events in one minute
 for attacker in ./find/*; do
 	events_counter 1
 	events_counter 2
